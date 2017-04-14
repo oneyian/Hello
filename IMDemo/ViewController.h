afbe4b622ae7d1a64d2998725e50d68db6e96299
John <1052394523@qq.com>
2017-04-14 09:55:20 +0800

    Default

---
+++ IMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IMDemo
+//
+//  Created by 王毅安 on 17/4/14.
+//  Copyright © 2017年 王毅安. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IMDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IMDemo
+//
+//  Created by 王毅安 on 17/4/14.
+//  Copyright © 2017年 王毅安. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
